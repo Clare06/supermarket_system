ac130f2f653bed4f6f92751eb47ca78cddc18467
Krishihan Raviendran <krishihan@Krishihans-MacBook-Pro.local>
2023-11-03 22:39:12 +0530

    Product service has been updated. Delete not working

--- product-service/src/main/java/com/programmingcodez/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/programmingcodez/productservice/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.programmingcodez.productservice.controller;
+import com.programmingcodez.productservice.dto.ProductRequest;
+import com.programmingcodez.productservice.dto.ProductResponse;
 import com.programmingcodez.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -11,7 +15,29 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
 
+    }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @DeleteMapping("/api/product/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteProduct(@PathVariable String id){
+        boolean deleted = productService.deleteProductById(id);
+
+        if(deleted){
+            return ResponseEntity.ok("Product deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found or could not be deleted");
+        }
+    }
 
 }
--- product-service/src/main/java/com/programmingcodez/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingcodez/productservice/service/ProductService.java
@@ -1,16 +1,61 @@
 package com.programmingcodez.productservice.service;
 
+import com.programmingcodez.productservice.dto.ProductRequest;
+import com.programmingcodez.productservice.dto.ProductResponse;
+import com.programmingcodez.productservice.entity.Product;
 import com.programmingcodez.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
-    private  ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+//        Creating instance of the product object
+
+        Product product = Product.builder()
+                .id(productRequest.getId())
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+//        save the product to the database
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
 
 
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
 
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    // Create object of productresponse
+    private ProductResponse mapToProductResponse(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+    //delete a product by ID
+    public boolean deleteProductById(String id) {
+
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
