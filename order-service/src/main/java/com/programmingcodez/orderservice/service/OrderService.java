6a738ee56c5156d74c689bec9645b8c9c6e70a33
Clare06 <106062267+Clare06@users.noreply.github.com>
2023-11-04 16:42:54 +0530

    placeorder api

--- order-service/src/main/java/com/programmingcodez/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingcodez/orderservice/service/OrderService.java
@@ -1,15 +1,50 @@
 package com.programmingcodez.orderservice.service;
 
+import com.programmingcodez.orderservice.dto.OrderLineItemsDto;
+import com.programmingcodez.orderservice.dto.OrderRequest;
+import com.programmingcodez.orderservice.entity.Order;
+import com.programmingcodez.orderservice.entity.OrderLineItem;
 import com.programmingcodez.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @Service
 @RequiredArgsConstructor
 
 public class OrderService {
-
+    @Autowired
     private final OrderRepository orderRepository;
+
+    public String placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItem> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItem::getSkuCode)
+                .toList();
+        orderRepository.save(order);
+        return skuCodes.toString()+orderLineItems.toString();
+//        return skuCodes.toString();
+    }
+
+    private OrderLineItem mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItem orderLineItems = new OrderLineItem();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
 
 }
--- product-service/src/main/java/com/programmingcodez/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingcodez/productservice/service/ProductService.java
@@ -1,16 +1,46 @@
 package com.programmingcodez.productservice.service;
 
+import com.programmingcodez.productservice.dto.ProductRequest;
+import com.programmingcodez.productservice.dto.ProductResponse;
+import com.programmingcodez.productservice.entity.Product;
 import com.programmingcodez.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-
+    @Autowired
     private  ProductRepository productRepository;
 
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .id(productRequest.getSkuCode())
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+//        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 
 
 }
