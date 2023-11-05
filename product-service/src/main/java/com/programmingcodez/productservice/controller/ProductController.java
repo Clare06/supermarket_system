8e9097dd3a123b3df878636945c6306400b2c026
Krishihan Raviendran <krishihan@Krishihans-MacBook-Pro.local>
2023-11-05 22:49:29 +0530

    fixing conflicts

--- product-service/src/main/java/com/programmingcodez/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/programmingcodez/productservice/controller/ProductController.java
@@ -5,8 +5,8 @@ import com.programmingcodez.productservice.dto.ProductResponse;
 import com.programmingcodez.productservice.entity.Product;
 import com.programmingcodez.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +21,6 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
-        System.out.println("I have been called");
         productService.createProduct(productRequest);
     }
 
--- product-service/src/main/java/com/programmingcodez/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingcodez/productservice/service/ProductService.java
@@ -5,17 +5,15 @@ import com.programmingcodez.productservice.dto.ProductResponse;
 import com.programmingcodez.productservice.entity.Product;
 import com.programmingcodez.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ProductService {
-
-    private final ProductRepository productRepository;
+    @Autowired
+    private  ProductRepository productRepository;
 
     public void createProduct(ProductRequest productRequest) {
         // Creating instance of the product object
