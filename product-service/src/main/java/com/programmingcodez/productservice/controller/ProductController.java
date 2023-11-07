fb658f8a5d26bfb5a528ccf4f78f83993b4d9d50
Krishihan Raviendran <krishihan@Krishihans-MacBook-Pro.local>
2023-11-07 22:38:49 +0530

    mongo database error fixed

--- product-service/src/main/java/com/programmingcodez/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/programmingcodez/productservice/controller/ProductController.java
@@ -8,9 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
